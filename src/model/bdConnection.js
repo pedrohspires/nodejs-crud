fddf839e47f69d9b7ecaa5d27449e232db2fdeca
Pedro Henrique S. Pires <pedropires2603@gmail.com>
2022-02-17 08:37:13 -0300

    Connection and db configs abstracted

--- src/model/bdConnection.js
+++ src/model/bdConnection.js
@@ -1,30 +1,11 @@
 const mysql = require('mysql');
+const {HOST, USER, PASSWORD, DATABASE} = require('../../config.json');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'crud',
-    password: 'pedroh2603',
-    database: 'crud'
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE
 });
 
-function queryExec(query, callback){
-    if(!connection.socket)
-        connection.connect(); // Conexão ainda não existia;
-    
-    connection.query(query, (err, results, fields)=>{
-        if(err)
-            throw err;
-        callback(results);
-        connection.end();
-    });
-}
-
-function endConnection(){
-    if(connection.socket)
-        connection.end();
-}
-
-module.exports = {
-    queryExec,
-    endConnection
-}
+module.exports = connection;
