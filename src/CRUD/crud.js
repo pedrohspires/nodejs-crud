6ed00eb4c0cd8fa01a602378d40fb03e5f9af925
Pedro Henrique <pedropires2603@gmail.com>
2022-02-28 20:41:52 -0300

    Create crud.js: base file to organize routes

---
+++ src/CRUD/crud.js
@@ -0,0 +1,11 @@
+const createData = require('./createData');
+const readData = require('./readData');
+const updateData = require('./updateData');
+const deleteData = require('./deleteData');
+
+module.exports = {
+    createData,
+    readData,
+    updateData,
+    deleteData
+}
