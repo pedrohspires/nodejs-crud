7945d31b8c5186cb1d74a0168f8d1af49496a1c7
Pedro Henrique <pedropires2603@gmail.com>
2022-03-01 09:27:10 -0300

    Udate file: comments added

--- src/CRUD/crud.js
+++ src/CRUD/crud.js
@@ -3,9 +3,10 @@ const readData = require('./readData');
 const updateData = require('./updateData');
 const deleteData = require('./deleteData');
 
+// Object containing all the methods needed by crud.
 module.exports = {
-    createData,
-    readData,
-    updateData,
-    deleteData
+    createData, // create a new data in the database
+    readData, // read an existing data in the database
+    updateData, // update an existing data in the database
+    deleteData // delete an existing data in the database
 }
