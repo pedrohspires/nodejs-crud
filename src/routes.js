7481fa01a3f21941d08c98c331bef19fa9c33377
Pedro Henrique <pedropires2603@gmail.com>
2022-03-01 09:26:14 -0300

    Update file: Try carch implementation

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 const { createData, readData, updateData, deleteData } = require('./CRUD/crud'); // CRUD unstructured functions
 
 module.exports = async (req, res) => {
+    // Routes method doesn't check for errors. It just executes the methods and makes the connection to the database.
     try{
+        // methods create errors if necessary to handle in catch
         switch (req.method) {
             case "POST": createData(req); break;
             case "GET": readData(req); break;
             case "PUT": updateData(req); break;
             case "DELETE": deleteData(req); break;
+            default: throw new Error("Unknown method");
         }
     }catch(err){
-        // erros que podem ocorrer ao tentar executar uma rota
+        // handles errors created in route methods
     }
 }
