e0fe005b9877332fc29958d34c173dad61d250cc
Pedro Henrique <pedropires2603@gmail.com>
2022-03-01 09:36:43 -0300

    Update app.js: server started message added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const server = require('http');
 const routes = require('./routes'); // routes is a function to handle requests
 
+console.log('Server started');
 server.createServer(routes).listen(3000);
