376685bf626a4f958305d8f996550f2ed8e9471a
Pedro Henrique <pedropires2603@gmail.com>
2022-03-06 10:07:16 -0300

    Update: database upgraded to mysql2

--- src/database.js
+++ src/database.js
@@ -1,8 +1,19 @@
-const config = require('../config.json');
-const mysql = require('mysql');
+async function connect(){
+    if(global.connection && global.connection.state !== 'disconnected')
+        return global.connection;
+    
+    const config = require('../config.json');
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({
+        host: config.HOST,
+        user: config.USER,
+        password: config.PASSWORD,
+        database: config.DATABASE
+    });
+    global.connection = connection;
+    return connection;
+}
 
-module.exports = mysql.createConnection({
-    host: config.HOST,
-    user: config.USER,
-    password: config.PASSWORD
-});
+module.exports = {
+    connect: connect
+};
