b67ab29109cb8b440dbf41dcf94bd26faeeb4614
Pedro Henrique <pedropires2603@gmail.com>
2022-03-06 10:12:37 -0300

    Update: added api route verification and modified the way routes are handled

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,34 @@
 const { createData, readData, updateData, deleteData } = require('./CRUD/crud'); // CRUD unstructured functions
+const database = require('./database');
+const requestResolve = require('./requestResolve');
+
+function apiPathValidator(url){
+    let validPath = '/api/';
+    for(let i in url)
+        if(url[i] != validPath[i])
+            return false;
+    return true;
+}
 
 module.exports = async (req, res) => {
     // Routes method doesn't check for errors. It just executes the methods and makes the connection to the database.
+    const url = requestResolve.path(req);
     try{
+        // Throws an error for a path other than /api
+        if(apiPathValidator(url))
+            throw new Error ("Path is not /api");
+
         // methods create errors if necessary to handle in catch
-        switch (req.method) {
-            case "POST": createData(req); break;
-            case "GET": readData(req); break;
-            case "PUT": updateData(req); break;
-            case "DELETE": deleteData(req); break;
-            default: throw new Error("Unknown method");
+        switch (url) {
+            case "/api/create": createData(req, database); break;
+            case "/api/read": readData(req, database); break;
+            case "/api/update": updateData(req, database); break;
+            case "/api/delete": deleteData(req, database); break;
+            default: throw new Error("Unknown path");
         }
     }catch(err){
+        console.log(err);
         // handles errors created in route methods
     }
+    res.end();
 }
