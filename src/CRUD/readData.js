78a82b4c7913e527655cc86392bde88f01e567f7
Pedro Henrique <pedropires2603@gmail.com>
2022-03-06 18:25:12 -0300

    Implements: the read route

--- src/CRUD/readData.js
+++ src/CRUD/readData.js
@@ -1,3 +1,25 @@
-module.exports = (req) => {
-    console.log('aqui');
+const database = require('../database');
+
+/**
+ * Execute the query in the database
+ * 
+ * @returns all rows of the database
+ */
+async function queryExec(){
+    const connection = await database.connect();
+    const [rows] = await connection.query('select * from product;');
+    return rows;
+}
+
+/**
+ * Read all products in the database
+ * 
+ * @param {object} req - http request object
+ * @param {object} res - http response object
+ */
+module.exports = async (req, res) => {
+    if(req.method !== 'GET')
+        throw "Method not supported";
+    
+    res.write(JSON.stringify(await queryExec()));
 }
